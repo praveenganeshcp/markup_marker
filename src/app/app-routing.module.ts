84443ed0caf106130388afbb4da06a2037ad6a3b
Praveen Kumar <praveen@textmercato.com>
2022-02-13 22:21:33 +0530

    route setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../app/modules/core/core.module').then(m => m.CoreModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BuilderComponent } from './pages/builder/builder.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BuilderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
