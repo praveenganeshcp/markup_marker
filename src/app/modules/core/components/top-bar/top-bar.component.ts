a3a7788f0eeffc17cab04c2c54d49ce33b827e29
praveen kumar <praveen@textmercato.com>
2022-02-27 06:38:29 +0530

    help icon added

--- src/app/modules/core/components/top-bar/top-bar.component.ts
+++ src/app/modules/core/components/top-bar/top-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DialogService } from 'src/app/modules/shared/services/dialog/dialog.service';
 import { ConfigService } from 'src/app/services/config/config.service';
+import { HelpBannerComponent } from '../help-banner/help-banner.component';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,13 +11,20 @@ import { ConfigService } from 'src/app/services/config/config.service';
 export class TopBarComponent implements OnInit {
 
   @Output() generateCode = new EventEmitter();
-  constructor(public configService: ConfigService) { }
+  constructor(public configService: ConfigService, private dialogService: DialogService) { }
 
   ngOnInit(): void {
   }
 
   generateOutput() {
     this.generateCode.emit();
+  }
+
+  openHelpBanner() {
+    this.dialogService.open(HelpBannerComponent, 60, 60, {
+        data: {},
+        isClosable: true
+    })
   }
 
 }
