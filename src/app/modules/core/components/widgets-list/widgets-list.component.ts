edcbda8c931d37e50a01e16ac8d443eaed251334
praveen kumar <praveen@textmercato.com>
2022-02-27 19:30:01 +0530

    bug fixed

--- src/app/modules/core/components/widgets-list/widgets-list.component.ts
+++ src/app/modules/core/components/widgets-list/widgets-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { HtmlResolverService } from '../../services/html-resolver/html-resolver.service';
+import { WidgetGroup } from '../../services/widgets-list/widget-group.model';
 import { WidgetsListService } from '../../services/widgets-list/widgets-list.service';
 
 @Component({
@@ -10,10 +11,13 @@ import { WidgetsListService } from '../../services/widgets-list/widgets-list.ser
 export class WidgetsListComponent implements OnInit {
 
   @Output() createWidget = new EventEmitter<HTMLElement>();
-  constructor(private htmlResolver: HtmlResolverService, private widgetsListService: WidgetsListService) { }
+  private widgetGroups: WidgetGroup[];
+  constructor(private htmlResolver: HtmlResolverService, private widgetsListService: WidgetsListService) { 
+    this.widgetGroups = this.widgetsListService.getWidgetsList();
+  }
 
   getWidgetGroups() {
-    return this.widgetsListService.getWidgetsList();
+    return this.widgetGroups;
   }
 
   ngOnInit(): void {
